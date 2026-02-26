b99e0248172d8907662350fbc369f403f972dbc9
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-02-27 01:51:01 +0530

    Update app.jsx to solve the conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
+import React from 'react';
 import Hero from './components/Hero';
 
 function App() {
   return (
-
     <div className="bg-white min-h-screen">
-      
       <main className="relative">
         <div className="pt-5"> 
           <Hero />
-
         </div>
       </main>
     </div>
