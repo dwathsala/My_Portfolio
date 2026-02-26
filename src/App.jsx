865b1068fd64c98b9661d35e1bce65b8a3f9f3c2
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-02-27 01:09:27 +0530

    Refactor App component for improved code clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import Header from './components/Header';
+import Header from './components/Header'; // Is this line here?
 import Hero from './components/Hero';
-
 
 function App() {
   return (
-
-    <div >
-      <Header />
-      <main className="relative">
+    <div>
+      <Header /> 
+      <main className='relative'>
         <div className="pt-5"> 
           <Hero />
-
         </div>
       </main>
     </div>
   );
 }
-
-export default App;
