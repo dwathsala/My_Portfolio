d9414e50cbaa71e5dc66f3e3cf4a822de2708b50
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-02-27 01:21:47 +0530

    Refactor App component for improved structure and clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import Header from './components/Header'; // Is this line here?
+import Header from './components/Header';
 import Hero from './components/Hero';
 
 function App() {
   return (
-    <div>
-      <Header /> 
-      <main className='relative'>
+    <div className="bg-white min-h-screen">
+      {/* The Header is global. It stays here forever. */}
+      <Header />
+      
+      <main className="relative">
+        
         <div className="pt-5"> 
           <Hero />
+          
         </div>
       </main>
     </div>
   );
 }
+
+export default App;
