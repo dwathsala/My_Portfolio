b9dfbb70d5bf9f0fd8133cd9535984b1176e2f25
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-02-27 09:55:38 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Hero from './components/Hero';
 function App() {
   return (
     <div className="bg-white min-h-screen">
+      {/* The Header is global. It stays here forever. */}
+      <Header />
       
       <main className="relative">
         
-        <div className=""> 
+        <div className="pt-10"> 
           <Hero />
           
         </div>
