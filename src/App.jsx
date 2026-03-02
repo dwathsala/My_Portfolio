28cef015b7ed0a4110cee1fa79b13377293e19e8
Dulari Dayananda <dulariwathsala824@gmail.com>
2026-03-02 19:32:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
-import About from './components/about';
+import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
